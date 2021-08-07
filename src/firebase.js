395d109f395a7f413d9aab3d9b9a898a9d1acbf5
NagwataEzekiel <69932561+NagwataEzekiel@users.noreply.github.com>
2021-08-08 02:53:02 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase"
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCT_kzBp-F-5AHSV2ig7cusOC7G4cSEw4w",
-    authDomain: "stack-online.firebaseapp.com",
-    projectId: "stack-online",
-    storageBucket: "stack-online.appspot.com",
-    messagingSenderId: "950055917473",
-    appId: "1:950055917473:web:1fd1114325c7f650ca1de0",
-    measurementId: "G-8P9SLPT7W7"
+    
   });
 
   const db = firebaseApp.firestore();
